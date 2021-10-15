10ca13263bf5117df38f922f0e26ac34dc061a08
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2021-10-15 14:21:09 -0300

    feat: functional login

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "https://kabit-api.herokuapp.com",
+const api = axios.create({
+  baseURL: "https://kenzie-habits.herokuapp.com",
 });
+
+export default api;
